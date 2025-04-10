348a71fb105602468c4121b88783579de3cbfcee
Himanshu Soni <himanshusoni19222@gmail.com>
2025-04-10 20:04:14 +0530

    chore: resolve dependency conflicts in project.clj
    Add exclusions to project.clj to resolve version conflicts for Guava, OAuth, and others.

--- project.clj
+++ project.clj
@@ -4,11 +4,17 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.google.api-client/google-api-client "2.7.2"]
-                 [com.google.oauth-client/google-oauth-client-jetty "1.39.0"]
+                 [com.google.api-client/google-api-client "2.7.2"
+                  :exclusions [com.google.guava/guava
+                               com.google.errorprone/error_prone_annotations
+                               org.checkerframework/checker-qual]]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.39.0"
+                  :exclusions [com.google.oauth-client/google-oauth-client]]
+                 [com.google.apis/google-api-services-sheets "v4-rev20250211-2.0.0"]
                  [cheshire/cheshire "5.13.0"]
                  [clj-http "3.13.0"]]
   :main ^:skip-aot finance-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[com.google.guava/guava "33.3.1-android"]]}})
