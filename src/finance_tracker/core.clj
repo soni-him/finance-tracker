9a629c3629ad4e1a2e28f3b4896f2b6a9179a9c4
Himanshu Soni <himanshusoni19222@gmail.com>
2025-04-11 00:46:50 +0530

    chore: remove unused regex parsing functions
    Drop transaction-type, extract-amount, extract-date, and extract-info after switching to Open AI.

--- src/finance_tracker/core.clj
+++ src/finance_tracker/core.clj
@@ -119,31 +119,6 @@
       (println (str "ERROR: Failed to build Google Sheets service: " (.getMessage e)))
       (throw e))))
 
-;; Step 1: Identify Transaction Type
-(defn transaction-type [text]
-  (cond
-    (re-find #"credited" text) :credit
-    (re-find #"debited" text) :debit
-    :else :unknown))
-
-;; Step 2: Extract Relevant Data
-(defn extract-amount [text]
-  (when-let [[_ amount] (re-find #"Rs\. (\d+(\.\d+)?)" text)]
-    {:amount (Double/parseDouble amount)}))
-
-(defn extract-date [text]
-  (when-let [[_ date] (re-find #"on (\d{1,2}-[A-Za-z]{3})" text)]
-    {:date date}))
-
-;; Step 3: Apply Only Relevant Extraction Functions
-(defn extract-info [text]
-  (let [type (transaction-type text)
-        extraction-fns (cond
-                         (= type :debit)  [extract-amount extract-date]
-                         (= type :credit) [extract-amount extract-date]
-                         :else [])]
-    (merge {:type type}
-           (apply merge (map #(% text) extraction-fns)))))
 
 ;; extracting data using open ai api
 
